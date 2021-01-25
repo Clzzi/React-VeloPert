d67a7fb5755f28324e41e8f9cd006463ee2680ec
Clzzi <sonmin1407@dgsw.hs.kr>
2021-01-25 18:02:53 +0900

    [ADD] - FULLWIDTH & OUTLINE STYLING

--- SASS example/src/App.js
+++ SASS example/src/App.js
@@ -6,19 +6,57 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button size = "large">Button</Button>
-        <Button size = "medium">Button</Button>
-        <Button size = "small">Button</Button>
+        <Button size="large">Button</Button>
+        <Button size="medium">Button</Button>
+        <Button size="small">Button</Button>
       </div>
+
       <div className="buttons">
-        <Button color = "gray" size = "large">Button</Button>
-        <Button color = "gray" size = "medium">Button</Button>
-        <Button color = "gray" size = "small">Button</Button>
+        <Button color="gray" size="large">
+          Button
+        </Button>
+        <Button color="gray" size="medium">
+          Button
+        </Button>
+        <Button color="gray" size="small">
+          Button
+        </Button>
       </div>
+
       <div className="buttons">
-        <Button color="pink" size = "large">Button</Button>
-        <Button color="pink" size = "medium">Button</Button>
-        <Button color="pink" size = "small">Button</Button>
+        <Button color="pink" size="large">
+          Button
+        </Button>
+        <Button color="pink" size="medium">
+          Button
+        </Button>
+        <Button color="pink" size="small">
+          Button
+        </Button>
+      </div>
+
+      <div className="buttons">
+        <Button color="blue" size="large" outline>
+          Button
+        </Button>
+        <Button color="pink" size="medium" outline>
+          Button
+        </Button>
+        <Button color="gray" size="small" outline>
+          Button
+        </Button>
+      </div>
+
+      <div className="buttons">
+        <Button color="blue" size="large" fullWidth>
+          Button
+        </Button>
+        <Button color="pink" size="large" fullWidth>
+          Button
+        </Button>
+        <Button color="gray" size="large" fullWidth>
+          Button
+        </Button>
       </div>
     </div>
   );
