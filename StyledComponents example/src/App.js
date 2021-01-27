be0463a09c69012f86089e407562768c9dd57c5e
Clzzi <sonmin1407@dgsw.hs.kr>
2021-01-27 17:50:12 +0900

    [ADD] - DIALOG

--- StyledComponents example/src/App.js
+++ StyledComponents example/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Button from './components/Button';
+import Dialog from './components/Dialog';
 
 const AppBlock = styled.div`
   width: 512px;
@@ -23,43 +24,68 @@ const palette = {
 };
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+  const onConfirm = () => {
+    setDialog(false);
+  };
+  const onCancle = () => {
+    setDialog(false);
+  };
   return (
     <ThemeProvider
       theme={{
         palette,
       }}
     >
-      <AppBlock>
-        <ButtonGroup>
-          <Button size="large">Button</Button>
-          <Button color="gray">Button</Button>
-          <Button color="pink" size="small">
-            Button
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button size="large">Button</Button>
+            <Button color="gray">Button</Button>
+            <Button color="pink" size="small">
+              Button
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" outline>
+              Button
+            </Button>
+            <Button color="gray" outline>
+              Button
+            </Button>
+            <Button color="pink" size="small" outline>
+              Button
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" fullWidth>
+              Button
+            </Button>
+            <Button color="gray" size="large" fullWidth>
+              Button
+            </Button>
+            <Button color="pink" size="large" fullWidth>
+              Button
+            </Button>
+          </ButtonGroup>
+          <Button color="pink" size="large" fullWidth onClick={onClick}>
+            삭제
           </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size="large" outline>
-            Button
-          </Button>
-          <Button color="gray" outline>
-            Button
-          </Button>
-          <Button color="pink" size="small" outline>
-            Button
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size="large" fullWidth>
-            Button
-          </Button>
-          <Button color="gray" size="large" fullWidth>
-            Button
-          </Button>
-          <Button color="pink" size="large" fullWidth>
-            Button
-          </Button>
-        </ButtonGroup>
-      </AppBlock>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancleText="취소"
+          onConfirm={onConfirm}
+          onCancle={onCancle}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
