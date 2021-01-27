9b4cd9a421a052dad8e73d256750fae61f8d9a3f
Clzzi <sonmin1407@dgsw.hs.kr>
2021-01-27 17:05:23 +0900

    [ADD] - BUTTON 3COLORS

--- StyledComponents example/src/App.js
+++ StyledComponents example/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import Button from './components/Button';
 
-const Circle = styled.div`
-width: 5rem;
-height: 5rem;
-background: black;
-border-radius: 50%;
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 
+const palette = {
+  blue: '#228be6',
+  gray: '#496057',
+  pink: '#f06595',
+};
+
 function App() {
-    return(
-        <Circle/>
-    );
+  return (
+    <ThemeProvider
+      theme={{
+        palette,
+      }}
+    >
+      <AppBlock>
+        <Button>Button</Button>
+        <Button color='gray'>Button</Button>
+        <Button color='pink'>Button</Button>
+      </AppBlock>
+    </ThemeProvider>
+  );
 }
 
 export default App;
