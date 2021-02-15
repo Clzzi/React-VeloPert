204b32f41d11bff89130eb9b670a5f533c78492a
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-15 17:06:14 +0900

    [ADD] - SETTING ROUTER

--- Redux-MiddleWare-Example/src/index.js
+++ Redux-MiddleWare-Example/src/index.js
@@ -9,6 +9,7 @@ import rootReducer from './modules';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(
   rootReducer,
@@ -16,11 +17,13 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 
