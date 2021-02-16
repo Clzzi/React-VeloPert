5bf67943e904f1dd02026ef7fde8cd1dff40cd4e
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-16 16:54:15 +0900

    [FIX] - RERODING ERROR

--- Redux-MiddleWare-Example/src/containers/PostContainer.js
+++ Redux-MiddleWare-Example/src/containers/PostContainer.js
@@ -1,24 +1,25 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
+import { reducerUtils } from '../lib/asyncUtils';
 import { getPost } from '../modules/posts';
 
-
 function PostContainer({ postId }) {
-  const {data, loading, error} = useSelector(state => state.posts.post);
+  const { data, loading, error } = useSelector(
+    (state) => state.posts.post[postId] || reducerUtils.initial(),
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if(data) return;
     dispatch(getPost(postId));
-  }, [postId, dispatch]);
+  }, [postId, dispatch, data]);
 
-  if(loading) return <div>로딩중...</div>
-  if(error) return <div>에러 발생!</div>
-  if(!data) return null;
+  if (loading && !data) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
 
-    return(
-        <Post post={data} />
-    );
+  return <Post post={data} />;
 }
 
 export default PostContainer;
