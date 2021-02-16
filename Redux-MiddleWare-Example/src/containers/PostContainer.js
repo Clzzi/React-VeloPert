883d6822c8f237f23387b40c53b3451225cda6e5
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-16 17:52:44 +0900

    [ADD] - REDUX WITH ROUTER(HISTORY)

--- Redux-MiddleWare-Example/src/containers/PostContainer.js
+++ Redux-MiddleWare-Example/src/containers/PostContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
 import { reducerUtils } from '../lib/asyncUtils';
-import { getPost } from '../modules/posts';
+import { getPost, goToHome } from '../modules/posts';
 
 function PostContainer({ postId }) {
   const { data, loading, error } = useSelector(
@@ -11,7 +11,7 @@ function PostContainer({ postId }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(data) return;
+    if (data) return;
     dispatch(getPost(postId));
   }, [postId, dispatch, data]);
 
@@ -19,7 +19,18 @@ function PostContainer({ postId }) {
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
-  return <Post post={data} />;
+  return (
+    <>
+      <button
+        onClick={() => {
+          dispatch(goToHome());
+        }}
+      >
+        홈 으로 이동
+      </button>
+      <Post post={data} />
+    </>
+  );
 }
 
 export default PostContainer;
