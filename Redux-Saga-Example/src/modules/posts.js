c300419d3752c5cb187647ccde65ad56a120efe4
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-18 18:48:59 +0900

    [ADD] - INTERGRATE  POSTS MODULES USING REDUX-SAGA (DELAY, PUT, CALL , TAKEEVERY, TAKEREADING, TAKELATEST)

--- Redux-Saga-Example/src/modules/posts.js
+++ Redux-Saga-Example/src/modules/posts.js
@@ -6,6 +6,7 @@ import {
   handleAsyncActionsById,
   reducerUtils,
 } from '../lib/asyncUtils';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -17,11 +18,57 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 
 const CLEAR_POST = 'CLEAR_POST';
 
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
-export const goToHome = () => (dispatch, getState, {history}) => {
-  history.push('/');
+export const getPosts = () => ({ type: GET_POSTS });
+
+export const getPost = (id) => ({
+  type: GET_POST,
+  payload: id,
+  meta: id,
+});
+
+function* getPostsSaga() {
+  try {
+    const posts = yield call(postsAPI.getPosts);
+    yield put({
+      type: GET_POSTS_SUCCESS,
+      payload: posts,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POSTS_ERROR,
+      payload: e,
+      error: true,
+    });
+  }
 }
+
+function* getPostSaga(action) {
+  const id = action.payload;
+  try {
+    const post = yield call(postsAPI.getPostById, id);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post,
+      meta: id,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_ERROR,
+      payload: e,
+      error: true,
+      meta: id,
+    });
+  }
+}
+
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+};
+
+export const goToHome = () => (dispatch, getState, { history }) => {
+  history.push('/');
+};
 
 export const clearPost = () => ({ type: CLEAR_POST });
 
