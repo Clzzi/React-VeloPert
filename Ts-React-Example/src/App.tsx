a6b56c59506b12b128303df5ba7c183105b6a22b
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-20 16:18:46 +0900

    [ADD] - LEARN TS TYPE USING REDUCER

--- Ts-React-Example/src/App.tsx
+++ Ts-React-Example/src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Counter from "./Counter";
-import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 function App() {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 }
 
 export default App;
