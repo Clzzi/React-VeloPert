d61325ba9003746215022c439ad4307469db07dc
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-20 16:35:57 +0900

    [ADD] - CONTEXTAPI REDUCER

--- Ts-React-Example/src/App.tsx
+++ Ts-React-Example/src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Counter from "./Counter";
 import ReducerSample from "./ReducerSample";
+import { SampleProvider } from "./SampleContext";
 
 function App() {
-  return <ReducerSample />;
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
 }
 
 export default App;
