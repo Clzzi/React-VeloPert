5c7ecc7c7fdd5682cd97e825a1c28adf6b4ee2f4
Clzzi <sonmin1407@dgsw.hs.kr>
2021-02-21 16:27:12 +0900

    [ADD] - INTEGRATE COUNTER EX USING REDUX WITH TS-REACT

---
+++ Ts-React with Redux Example/src/modules/counter.ts
@@ -0,0 +1,41 @@
+const INCREASE = 'counter/INCREASE' as const;
+const DECREASE = 'counter/DECREASE' as const;
+const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const increaseBy = (diff: number) => ({
+  type: INCREASE_BY,
+  payload: diff,
+});
+
+type counterState = {
+  count: number;
+};
+
+const initialState: counterState = {
+  count: 0,
+};
+
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+function counter(
+  state: counterState = initialState,
+  action: CounterAction,
+): counterState {
+  switch (action.type) {
+    case 'counter/INCREASE':
+      return { count: state.count + 1 };
+    case 'counter/DECREASE':
+      return { count: state.count - 1 };
+    case 'counter/INCREASE_BY':
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
